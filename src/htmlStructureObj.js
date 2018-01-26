710fa3d8ce85b562637bd4604261add8f47fb6e9
Łukasz Kuciel <lukaszkuciel95@gmail.com>
2018-01-26 19:12:55 +0100

    add this script to your html
    This plugin can create JS & JSON object from current html where it is placed.

---
+++ src/htmlStructureObj.js
@@ -0,0 +1,17 @@
+//creates array of all elements in document//
+let htmlDOM = $(document).find('*').toArray();
+
+
+let htmlRecursion = (nodes, parent) => {
+  let structure = {};
+  nodes
+    .filter(n => n.parentNode === parent)
+    .forEach(n => structure[n.localName] = htmlRecursion(htmlDOM, n));
+  return structure;
+}
+
+//logs JSON object with html structure//
+console.log( JSON.stringify( htmlRecursion(htmlDOM, document), null, 2 ));
+
+//logs Js object with html structure//
+console.log( htmlRecursion(htmlDOM, document));
